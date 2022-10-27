e3b20457bd0d6d0dc9f6f23ce281e80cdbe38f71
Kenneth Lee <kenneth.lee.developer@gmail.com>
2022-10-27 16:14:05 -0700

    Create express app

---
+++ backend/routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+const createHttpError = require('http-errors');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
